b9d2d8d5e230f6fe77c32476007242404ed7c083
Lucas Inocencio Pires <piresilucas@gmail.com>
2019-12-09 23:13:12 -0300

    Add more function of validations

--- script/main.js
+++ script/main.js
@@ -5,6 +5,10 @@ function init() {
   form.addEventListener('submit', validationForm, false);
   form.cpf.addEventListener('blur', validationCPF, false);
   form.email.addEventListener('blur', validationEmail, false);
+  form.name.addEventListener('blur', validationName, false);
+  form.personalPhone.addEventListener('blur', validationPhone, false);
+  form.comercialPhone.addEventListener('blur', validationComercialPhone, false);
+  form.birthDate.addEventListener('blur', validationAge, false);
 }
 
 function addClassError(field) {
@@ -51,11 +55,65 @@ function validationCPF() {
   return true;
 }
 
+function validationName() {
+  let field = document.forms['register'].name;
+  let inputName = field.value; 
+  if(inputName == null || inputName == '' || inputName.length <= 2 || inputName.length > 30) {
+    console.log("add class error input name");
+    addClassError(field);
+    return false;
+  }
+  removeClassError(field);
+  return true;
+}
+
+function validationAge() {
+  let field = document.forms['register'].birthDate;
+  let inputAge = field.value;
+  if(inputAge == null || inputAge == '' || parseInt(inputAge) < 18 || parseInt(inputAge) > 110) {
+    console.log("add class erro input age");
+    addClassError(field);
+    return false;
+  }
+  removeClassError(field);
+  return true;
+}
+
+function validatePhone(phone) {
+  let regex = /\d{2}\d{4,5}\d{4}/g;
+  return regex.test(phone);
+}
+
+function validationComercialPhone() {
+  let field = document.forms['register'].comercialPhone;
+  let inputPhone = field.value;
+  if(inputPhone == null || inputPhone == '' || validatePhone(inputPhone) == false) {
+    console.log("add class erro input email");
+    addClassError(field);
+    return false;
+  }
+  removeClassError(field);
+  return true;
+}
+
+function validationPhone() {
+  let field = document.forms['register'].personalPhone;
+  let inputPhone = field.value;
+  if(inputPhone == null || inputPhone == '' || validatePhone(inputPhone) == false) {
+    console.log("add class erro input email");
+    addClassError(field);
+    return false;
+  }
+  removeClassError(field);
+  return true;
+}
+
 function validationForm(event) {
   console.log("validationForm");
-
   validationCPF();
   validationEmail();
+  validationAge();
+  validationPhone();
 
   event.preventDefault();
 }
